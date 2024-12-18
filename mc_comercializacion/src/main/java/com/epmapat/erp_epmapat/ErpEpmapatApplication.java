02ec5ffd52264aa10db08e1d8166bbab0fe29b71
ortizln <ortizln9@gmail.com>
2024-12-18 12:31:03 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ErpEpmapatApplication.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ErpEpmapatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ErpEpmapatApplication extends SpringBootServletInitializer {
+public class ErpEpmapatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ErpEpmapatApplication.class, args);
 	}
