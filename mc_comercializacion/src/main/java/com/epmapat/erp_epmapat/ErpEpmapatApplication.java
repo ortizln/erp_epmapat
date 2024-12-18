830a899f5093ba6ccbe0e13d3b91166d0d089c1d
ortizln <ortizln9@gmail.com>
2024-12-18 15:29:04 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ErpEpmapatApplication.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ErpEpmapatApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComercializacionApplication {
+public class ErpEpmapatApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ComercializacionApplication.class, args);
+		SpringApplication.run(ErpEpmapatApplication.class, args);
 	}
 
 }
