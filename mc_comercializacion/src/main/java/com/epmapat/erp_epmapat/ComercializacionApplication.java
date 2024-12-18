3e553fb630c8e4ae3bc1f94bd169d6673ef29949
ortizln <ortizln9@gmail.com>
2024-12-18 14:14:49 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
@@ -2,9 +2,17 @@ package com.epmapat.erp_epmapat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class ComercializacionApplication {
+public class ComercializacionApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ComercializacionApplication.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder aplication) {
+		return aplication.sources(ComercializacionApplication.class);
+	}
+
 }
