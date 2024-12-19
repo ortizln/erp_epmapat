361d7dd304c5b8b3f0d2e2b876538175f26ed232
ortizln <ortizln9@gmail.com>
2024-12-19 11:15:05 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/config/RestTemplateConfig.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.epmapat.erp_epmapat.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
     }
     
 }
