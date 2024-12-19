0ba09211ab933bd1812dd212acc9e35ca427cbb9
ortizln <ortizln9@gmail.com>
2024-12-19 11:23:10 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/servicio/microservicio_recaudacion/RecaudacionMicroservice.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/servicio/microservicio_recaudacion/RecaudacionMicroservice.java
@@ -17,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class RecaudacionMicroservice {
 	@Autowired
 	private RestTemplate restTemplate;
-	String mcrecaudacion = "localhost:mc_recaudacion";
+	String mcrecaudacion = "localhost:mc_recaudacion:8082";
 	private final String URL_FACTURA = "http://"+mcrecaudacion+"/facturas";
 	private final String URL_CAJA = "http://"+mcrecaudacion+"/cajas";
 	private final String URL_INTERES = "http://"+mcrecaudacion+"/interes";
