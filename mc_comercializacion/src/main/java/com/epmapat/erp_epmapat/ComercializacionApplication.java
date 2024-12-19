2c1e6e4ab78c39783c992e9394ece77c542db235
ortizal19009 <ort19009@byui.edu>
2024-12-18 23:01:55 -0500

    update

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErpEpmapatApplication {
+public class ComercializacionApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ErpEpmapatApplication.class, args);
+		SpringApplication.run(ComercializacionApplication.class, args);
 	}
 
 }
