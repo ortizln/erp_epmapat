1b292d59addce7f58749068efaf8ca844a018564
ortizal19009 <ort19009@byui.edu>
2024-12-29 14:47:32 -0500

    CREATE END PONT FINALL CLIENTES

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/repositorio/Clientes_rep.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/repositorio/Clientes_rep.java
@@ -3,5 +3,5 @@ package com.epmapat.erp_epmapat.repositorio;
 import com.epmapat.erp_epmapat.modelo.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Clientes_rep extends JpaRepository<Long, Clientes> {
+public interface Clientes_rep extends JpaRepository<Clientes, Long> {
 }
