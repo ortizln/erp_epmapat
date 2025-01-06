317241f21f1341b02935f4f14d9c4478db1c20db
ortizln <ortizln9@gmail.com>
2025-01-06 12:21:48 -0500

    update Login microservice

--- mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
+++ mc_comercializacion/src/main/java/com/epmapat/erp_epmapat/ComercializacionApplication.java
@@ -2,8 +2,10 @@ package com.epmapat.erp_epmapat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ComercializacionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComercializacionApplication.class, args);
